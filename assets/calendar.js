4cc3cbe56f33a80bf6cbdc9f87421651ade0c3f1
1808561047_Ni Putu Novia Ardiyanti <58169170+noviaardiyanti10@users.noreply.github.com>
2020-10-31 20:49:45 +0800

    up

---
+++ assets/calendar.js
@@ -0,0 +1,11 @@
+var tanggallengkap = new String();
+var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+namahari = namahari.split(" ");
+var namabulan = ("Januari Februari Maret April Mei Juni Juli Agustus September Oktober November Desember");
+namabulan = namabulan.split(" ");
+var tgl = new Date();
+var hari = tgl.getDay();
+var tanggal = tgl.getDate();
+var bulan = tgl.getMonth();
+var tahun = tgl.getFullYear();
+
